b933241238bb891d6ca4ea51e1980c958fe47e82
Ramneek-dev <68768431+Ramneek-dev@users.noreply.github.com>
2020-07-25 15:08:24 +0530

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+## 0.0.1
+
+* Fully working version without tests.
+
+## 0.0.2
+
+* Fixing Dart Pub link.
+
+## 0.0.3
+
+* Ability to showing all the numbers around the clock and bumping up the kotlin version thanks to @pidmid
+
+
+## 0.0.4
+
+* Migrating example project to AndroidX and fixing kotlin bug.
---
+++ example/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:analog_clock_example/demo.dart';
+import 'package:flutter/material.dart';
+
+import 'package:analog_clock/analog_clock.dart';
+
+void main() => runApp(MyApp());
+//void main() => runDemo();
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+
+          home: Scaffold(
+        body: AnalogClock(),
+      ));
+}
